b0eebce28ce81d1acda8537ee3fa5538cecd7d8c
artgarth1-afk <artgarth1@gmail.com>
2025-11-26 19:42:52 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+import { IfcViewerAPI } from '@ifcjs/viewer';
+
+const container = document.getElementById('viewer');
+const viewer = new IfcViewerAPI({ container, backgroundColor: 0xffffff });
+
+viewer.axes.setAxes();
+viewer.grid.setGrid();
+
+// Загружаем ваш файл из корня (рядом с index.html)
+viewer.IFC.loadIfcUrl('model.ifc');
