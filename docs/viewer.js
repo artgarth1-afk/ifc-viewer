4ac8ff6803af39dfe518632aca58281049013d05
artgarth1-afk <artgarth1@gmail.com>
2025-11-27 22:47:19 +0300

    Update viewer.js

--- docs/viewer.js
+++ docs/viewer.js
@@ -1,5 +1,11 @@
-// Импорт основных компонентов из xeokit-sdk
+// Импорт библиотек
+// 1. Импортируем Viewer и WebIFCLoaderPlugin из Xeokit
 import { Viewer, WebIFCLoaderPlugin } from "./xeokit-sdk.es.js";
+
+// 2. ИМПОРТИРУЕМ WebIFC API как модуль. 
+// Используем 'import * as', чтобы получить весь экспортированный объект.
+import * as WebIFC from "./web-ifc-api.js"; 
+
 
 // 1. Инициализация Viewer (сцена)
 const viewer = new Viewer({
@@ -12,19 +18,24 @@ viewer.camera.eye = [-3.93, 2.85, 27.01];
 viewer.camera.look = [4.40, 3.72, 8.89];
 viewer.camera.up = [-0.01, 0.99, 0.039];
 
+
 // 2. Настройка загрузчика IFC
-// ВАЖНО: передаем объект webIFC, который мы подключили в HTML
+// ИСПРАВЛЕНИЕ ОШИБКИ: передаем импортированный объект WebIFC
 const ifcLoader = new WebIFCLoaderPlugin(viewer, {
     wasmPath: "./",       // Путь к файлу .wasm
-    webIFC: window.WebIFC // Явная ссылка на библиотеку парсера
+    
+    // Передаем импортированный объект WebIFC в конфигурацию плагина
+    webIFC: WebIFC 
 });
+
 
 // 3. Загрузка модели
 const model = ifcLoader.load({
     id: "myModel",
     src: "./model.ifc",   // Имя вашего файла
-    edges: true           // Показывать грани (черные линии)
+    edges: true           // Показывать грани
 });
+
 
 // 4. События загрузки
 model.on("loaded", () => {
